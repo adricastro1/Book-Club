32cdf1b08854ad8c2ed0a951a7fd6f981e4e6478
adricastro1 <adricastro1196@gmail.com>
2023-04-18 11:11:35 -0400

    authentication set up

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.contrib.auth.models import User
 
 class Book(models.Model):
     title = models.CharField(max_length=100)
@@ -8,3 +10,10 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+    
+
+class Review(models.Model):
+    comment = models.TextField(max_length=500)
+    rating = models.IntegerField(default=0)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
