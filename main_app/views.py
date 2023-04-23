b09b14d2a52a746d92909471736f2756b4d3ed6a
adricastro1 <adricastro1196@gmail.com>
2023-04-23 19:36:45 -0400

    fix book_detail error 2

--- main_app/views.py
+++ main_app/views.py
@@ -26,18 +26,15 @@ def signup(request):
 
 
 def home(request):
-    book_of_the_month = Book.objects.get(id=2)
+    book_of_the_month = Book.objects.get(id=1)
     books = Book.objects.all().order_by('-id')
     return render(request, 'books/home.html', {'books': books, 'book_of_the_month': book_of_the_month})
 
 
 @login_required
 def readinglist(request):
-    try:
-        reading_list = ReadingList.objects.get(user=request.user)
-    except ReadingList.DoesNotExist:
-        reading_list = ReadingList.objects.create(user=request.user)
-    books_read, _ = BooksRead.objects.get_or_create(user=request.user)
+    reading_list, created = ReadingList.objects.get_or_create(user=request.user)
+    books_read, created = BooksRead.objects.get_or_create(user=request.user)
     book_list = reading_list.books.all().order_by('-id')
     read_list = books_read.books.all()
     return render(request, 'books/readinglist.html', {
@@ -52,10 +49,7 @@ def readinglist(request):
 
 @login_required
 def book_detail(request, book_id):
-    try:
-        reading_list = ReadingList.objects.get(user=request.user)
-    except ReadingList.DoesNotExist:
-        reading_list = ReadingList.objects.create(user=request.user)
+    reading_list, created = ReadingList.objects.get_or_create(user=request.user)
     book = Book.objects.get(id=book_id)
     review_form = ReviewForm()
     return render(request, 'books/detail.html', {'book': book, 'reading_list_id': reading_list.id, 'review_form': review_form})
